a66cd899efe11d109cc3d9b8b17e09096d8f769c
MrAhmed0702 <amochi670@rku.ac.in>
2025-11-30 07:44:05 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-4_Week-2-Project_Nov-30/',
+  base: '/Frontend-4_Week-2-Project_Nov-30/',
   plugins: [react()],
 })
